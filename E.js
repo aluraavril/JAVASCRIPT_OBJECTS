39095d9133d98745e6d18f116899f7e97f686f30
mango habanero <167891294+aluraavril@users.noreply.github.com>
2025-10-07 22:28:18 +0800

    Create E.js

---
+++ E.js
@@ -0,0 +1,12 @@
+const cafes = [
+  { name: "8shots Cafe", location: "Makati", rating: 4.6 },
+  { name: "Starbucks", location: "Taguig", rating: 4.8 },
+  { name: "Gong Cha", location: "Quezon City", rating: 4.4 },
+  { name: "But First, Coffee", location: "Pasig", rating: 4.7 },
+  { name: "Coco", location: "Cebu", rating: 4.5 }
+];
+
+const highRated = cafes.filter(cafe => cafe.rating >= 4.7);
+console.log("Cafes with rating 4.7 and above:", highRated);
+
+
