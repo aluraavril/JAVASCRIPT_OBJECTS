a1a8dc473f6db7875224748cd189ab47cbedf501
mango habanero <167891294+aluraavril@users.noreply.github.com>
2025-10-07 22:24:38 +0800

    Create C.js

---
+++ C.js
@@ -0,0 +1,10 @@
+const cafes = [
+  { name: "8shots Cafe", location: "Makati", rating: 4.6 },
+  { name: "Starbucks", location: "Taguig", rating: 4.8 },
+  { name: "Gong Cha", location: "Quezon City", rating: 4.4 },
+  { name: "But First, Coffee", location: "Pasig", rating: 4.7 },
+  { name: "Coco", location: "Cebu", rating: 4.5 }
+];
+
+cafes.push({ name: "Coffee Project", location: "Alabang", rating: 4.9 });
+console.log("Push Result:", cafes);
